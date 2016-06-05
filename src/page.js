9ac8803af209293299675c2670421fa3aba33aed
Mert <pvtmert@gmail.com>
2016-06-05 17:23:03 +0300

    updated: Sun Jun  5 17:23:01 EEST 2016

--- src/page.js
+++ src/page.js
@@ -1,5 +1,7 @@
 window.onload = function() {
 	fix_images();
 	fix_links();
+	footer = document.getElementsByTagName("footer")[0];
+	footer.innerHTML += window.atob("PGJyPgo8c3BhbiBzdHlsZT0nZm9udC1zaXplOjAuNWVtOyc+ZGV2ZWxvcG1lbnQgJmFtcDsgZGVzaWduOiBtZXJ0IGFrZW5naW48L3NwYW4+Cg==");
 	return;
 }
