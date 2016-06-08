74f9dff16af429bb6c4a6ed6b5f669394ff41e5c
Mert <pvtmert@gmail.com>
2016-06-08 19:34:22 +0300

    updated: Wed Jun  8 19:34:18 EEST 2016

--- src/img.js
+++ src/img.js
@@ -1,7 +1,10 @@
 function fix_images() {
 	var __imglist = document.querySelectorAll("[data-img]");
 	for (var i=0;i<__imglist.length;i++) {
-		var url = __imglist[i].getAttribute('data-img');
-		__imglist[i].style.backgroundImage="url('" + url + "')";
+		urls = __imglist[i].getAttribute('data-img').split(",");
+		urls = urls.join("'), url('");
+		prop = "url('" + urls + "')";
+		__imglist[i].style.backgroundImage = prop;
+		//__imglist[i].style.background = prop;
 	}
 };
